76963d4e3642338a83bccc99ec216299b0fd88b7
your_github_username <devenvaruv@gmail.com>
2023-08-18 11:12:16 -0700

    .

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -4,6 +4,12 @@ function About() {
   return (
     <div>
         <p>hello About</p>
+        <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa
+          egestas mollis varius; dignissim elementum. Mollis tincidunt mattis
+          hendrerit dolor eros enim, nisi ligula ornare. Hendrerit parturient
+          habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+          sodales taciti duis praesent id. Consequat urna vitae morbi nunc
+          congu.</p>
       
     </div>
   );
