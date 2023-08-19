a7d7b1760edb939d957defac3abd300fa2d1f1f2
your_github_username <devenvaruv@gmail.com>
2023-08-19 14:22:54 -0700

    updated

--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -1,10 +1,51 @@
-import './Services.css';
+import "./Services.css";
 
 function Services() {
   return (
     <div>
-        <p>hello Services</p>
-      
+      <div>
+        <h2>Our Services</h2>
+      </div>
+      <div>
+        <div>
+          <h4>What we Offer</h4>
+          <div>
+            <h4>Serivce 1</h4>
+            <p>
+              What should we know about this service? Describe important details
+              like price, value, length of service, and w hy it’s unique. Or use
+              these sections to showcase key values of your services.
+            </p>
+          </div>
+          <div>
+            <h4>Serivce 2</h4>
+            <p>
+              What should we know about this service? Describe important details
+              like price, value, length of service, and w hy it’s unique. Or use
+              these sections to showcase key values of your services.
+            </p>
+          </div>
+          <div>
+            <h4>Serivce 3</h4>
+            <p>
+              What should we know about this service? Describe important details
+              like price, value, length of service, and w hy it’s unique. Or use
+              these sections to showcase key values of your services.
+            </p>
+          </div>
+          <div>
+            <h4>Serivce 4</h4>
+            <p>
+              What should we know about this service? Describe important details
+              like price, value, length of service, and w hy it’s unique. Or use
+              these sections to showcase key values of your services.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        
+      </div>
     </div>
   );
 }
