7dfd1c745287d366bf3a96c45abce77fd5e041ac
your_github_username <devenvaruv@gmail.com>
2023-08-19 12:17:19 -0700

    updated about page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,16 +1,43 @@
-import './About.css';
+import "./About.css";
+import Demo from "../../assets/images/Demo.jpg";
 
 function About() {
   return (
-    <div>
-        <p>hello About</p>
-        <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa
+    <div className="about-container">
+      <div className="about-content">
+        <h2>Who am I</h2>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa
           egestas mollis varius; dignissim elementum. Mollis tincidunt mattis
           hendrerit dolor eros enim, nisi ligula ornare. Hendrerit parturient
           habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
           sodales taciti duis praesent id. Consequat urna vitae morbi nunc
-          congu.</p>
-      
+          congu.
+        </p>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa
+          egestas mollis varius; dignissim elementum. Mollis tincidunt mattis
+          hendrerit dolor eros enim, nisi ligula ornare. Hendrerit parturient
+          habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+          sodales taciti duis praesent id. Consequat urna vitae morbi nunc
+          congu.
+        </p>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Ac purus in massa
+          egestas mollis varius; dignissim elementum. Mollis tincidunt mattis
+          hendrerit dolor eros enim, nisi ligula ornare. Hendrerit parturient
+          habitant pharetra rutrum gravida porttitor eros feugiat. Mollis elit
+          sodales taciti duis praesent id. Consequat urna vitae morbi nunc
+          congu.
+        </p>
+      </div>
+      <div className="about-image">
+        <img
+          src={Demo}
+          srcSet={`${Demo} 1x, ${Demo} 2x`} // Provide different image sizes
+          alt="A beautiful landscape"
+        />
+      </div>
     </div>
   );
 }
